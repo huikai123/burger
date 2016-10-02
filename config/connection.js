021c8e293f1144ed65e8853c0f5e50d4cf78bd56
Kai <huikaizhong@yahoo.com>
2016-10-01 23:15:13 -0400

    add orm.js

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,13 @@
 Here is where you make the connection to the database and export and used by the O.R.M.
 */
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	port: 3306,
+var connection; 
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+
 	host: 'localhost',
 	user: 'root',
 	password: '',
