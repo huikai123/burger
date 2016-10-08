e0f7485c547a5f7be5fa0d59e22720ce7b5d24a8
Kai <huikaizhong@yahoo.com>
2016-10-08 18:52:02 -0400

    work on test.html&css&index.handlebars

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,7 +4,7 @@ LIMIT 0, 5000
 
 -- Date: 2016-10-01 18:07
 */
-INSERT INTO burgers (id, burger_name, devoured, date) VALUES (1,'turkey','2009-01-16 07:15:23');
+INSERT INTO burgers (id, burger_name, devoured, date) VALUES (1,'turkey', false,'2009-01-16 07:15:23');
 INSERT INTO burgers (id, burger_name, devoured, date) VALUES (2,'veggie', true,'2009-10-16 10:30:20');
-INSERT INTO burgers (id, burger_name, devoured, date) VALUES (3,'chips','2016-09-20 17:45:08');
+INSERT INTO burgers (id, burger_name, devoured, date) VALUES (3,'chips', false,'2016-09-20 17:45:08');
 INSERT INTO burgers (id, burger_name, devoured, date) VALUES (4,'ham', true ,'2016-10-01 20:27:53');
